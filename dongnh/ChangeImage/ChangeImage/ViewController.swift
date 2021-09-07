af7a4f8aa60f5e6ee080400aafbff172dc446726
Nguyen Huy Dong <>
2021-09-07 09:53:07 +0700

    changeImage

---
+++ dongnh/ChangeImage/ChangeImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeImage
+//
+//  Created by Nguyen Huy Dong on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var img: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
