5e37646f50c4ac9bea4903e059c7c1e1829f650e
Nguyen Huy Dong <>
2021-09-09 22:35:46 +0700

    update

--- dongnh/ChangeImage/ChangeImage/ViewController.swift
+++ dongnh/ChangeImage/ChangeImage/ViewController.swift
@@ -6,15 +6,140 @@
 //
 
 import UIKit
+import SSZipArchive
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SSZipArchiveDelegate,  URLSessionTaskDelegate,
+                      URLSessionDownloadDelegate {
+    
     @IBOutlet weak var img: UIImageView!
+    
+     let zipURL = "https://test-assets-mobile.s3-us-west-2.amazonaws.com/125%402.zip"
+     let imageFile = "25.png"
+     let zipURL2 = "https://test-assets-mobile.s3-us-west-2.amazonaws.com/127%402.zip"
+     let imageFile2 = "27.png"
+    
+    var zipPath: String?
+    
+    func unzipPath() -> String? {
+        let path = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0]
+        return path
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        clearImageView()
+        download()
+        updateImageView()
     }
-
-
+    
+    func clearImageView() {
+        img.image = nil
+    }
+    //  UpdateView
+    func updateImageView() {
+        let unzipPath = self.unzipPath()! as NSString
+        let imagePath = unzipPath.appendingPathComponent(imageFile)
+        let image = UIImage.init(contentsOfFile: imagePath)
+        img.image = image
+    }
+    
+    
+    
+    private func download() {
+        
+        //create session configuration
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .reloadIgnoringLocalCacheData // reload cache
+        
+        //create session
+        let session = URLSession.init(configuration: config,
+                                      delegate: self,
+                                      delegateQueue: OperationQueue.main)
+        
+        //download task
+        guard let url = URL.init(string: zipURL) else {
+            return
+        }
+        
+        let task = session.downloadTask(with: url)
+        
+        // Start downloading
+        task.resume()
+    }
 }
 
+extension ViewController {
+    
+    //delete file
+    func removeItem(_ path: String) {
+        guard FileManager.default.fileExists(atPath: path) else {
+            return
+        }
+        do {
+            try FileManager.default.removeItem(atPath: path)
+        }
+        catch _ {
+            print("removeItem error.")
+            return
+        }
+    }
+}
+
+
+// MARK: - URLSessionTaskDelegate
+extension ViewController {
+    
+    //session complete
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        print("URLSession didComplete")
+        session.finishTasksAndInvalidate()  // Session termination processing
+        guard error == nil else {
+            
+            // error
+            print("Error \(String(describing: error))")
+            return
+        }
+        
+        // success
+        print("Success")
+        if Thread.isMainThread {
+            updateImageView()
+        }
+        else {
+            DispatchQueue.main.sync { [weak self] in
+                updateImageView()
+            }
+        }
+    }
+}
+// MARK: - URLSessionDownloadDelegate
+extension ViewController {
+    
+    //download task process
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        // Check progress
+        print("\(totalBytesWritten) / \(totalBytesExpectedToWrite)")
+    }
+    
+    
+     // Download task completed
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        let unzipPath = self.unzipPath()
+        
+        // Delete the old destination folder
+        removeItem(unzipPath!)
+        
+        // Prepare the extraction folder
+        do {
+            try FileManager.default.createDirectory(atPath: unzipPath!, withIntermediateDirectories: true, attributes: nil)
+        }
+        catch _ {
+            print("createDirectory error.")
+            return
+        }
+        
+        // Unzip
+        SSZipArchive.unzipFile(atPath: location.path, toDestination: unzipPath!)
+    }
+}
